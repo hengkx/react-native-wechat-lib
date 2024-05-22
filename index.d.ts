50e69f04d022e8f3cf15203a11bc5f13b1f96a34
Tom Xu <ycxzhkx@gmail.com>
2024-05-22 13:43:35 +0800

    improve

--- index.d.ts
+++ index.d.ts
@@ -8,7 +8,7 @@ enum WXScene {
   WXSceneSpecifiedSession = 3 /**< 指定联系人  */,
 }
 
-declare module 'react-native-wechat-lib' {
+declare module '@hengkx/react-native-wechat-lib' {
   export function registerApp(appId: string, universalLink?: string): Promise<boolean>;
   export function isWXAppInstalled(): Promise<boolean>;
   export function isWXAppSupportApi(): Promise<boolean>;
